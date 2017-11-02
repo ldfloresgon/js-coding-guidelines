5c9b5b8d6ee7f3f570815d4b5b3b26cff264957a
Ruzafan <marcramiro@gmail.com>
2017-11-02 15:56:30 +0100

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,19 @@
+(function (exports) {
+var init = function(){
+
+},
+
+var initParam = function(param){
+
+}
+
+var module = {
+    testModule: {
+        init: init,
+        initParam: initParam
+    }
+};
+
+$.extend(exports, module);
+
+})(test);

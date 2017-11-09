046332f76bd6d3cb987f0b950461f920ac193a8d
Ruzafan <marcramiro@gmail.com>
2017-11-09 22:48:22 +0100

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,19 +1,47 @@
-(function (exports) {
-var init = function(){
+if(!myObject) myObject = {};
 
+(function (exports) {
+
+var name = "Jhon";
+var age = 35;
+var id = 1;
+var getName = function(){
+    return this.name;
+},
+    
+var getAge = function(){
+    return this.age;
+},
+ 
+var setName = function(name){
+    this.name = name;
+},
+    
+var setAge = function(age){
+    this.age = age;
 },
 
-var initParam = function(param){
-
-}
+var getId = function(){
+   return this.id;  
+},
 
 var module = {
-    testModule: {
-        init: init,
-        initParam: initParam
+    person: {
+        getName : getName,
+        getAge: getAge,
+        setName: setName,
+        setAge: setAge
     }
 };
 
 $.extend(exports, module);
 
-})(test);
+})(myObject);
+
+return myObject.person.getName(); //Jhon
+
+myObject.person.setName("Jhon Smith");
+return myObject.person.getName(); //Jhon Smith
+
+return myObject.person.getId(); //1
+myObject.person.id = 2; //error
